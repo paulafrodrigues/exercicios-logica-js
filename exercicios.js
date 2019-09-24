4b3d88f12ac801748c545b9f00f893d6570927d4
Paula Franco <pfr.blackcat@gmail.com>
2019-09-24 19:04:07 -0300

    first commit

---
+++ exercicios.js
@@ -0,0 +1,19 @@
+
+// 1. Faça uma mensagem de "Olá Mundo!"
+
+alert("Olá Mundo!");
+
+// 2. Faça uma saída de soma de 2 números.
+
+console.log(2+3);
+
+//3. Utilizando variáveis, solicite o ano de nascimento do usuário, calcule sua idade e mostre em um alert.
+var ano = (prompt("Que ano você nasceu"));
+alert(2019 - ano);
+
+// 4. Receba do usuários 3 números e calcule a média entre eles.
+var num1 = parseInt(prompt ("Digite um número qualquer"));
+var num2 = parseInt(prompt("Digite outro número"));
+var num3 = parseInt(prompt("Digite mais um número, disgraça"));
+var total = num1 + num2 + num3
+console.log(total / 3);
